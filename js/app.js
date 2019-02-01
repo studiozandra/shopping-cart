8e38a605177d34510d8b84deaebaba09a65cd262
studiozandra <zan@studiozandra.com>
2019-02-01 09:28:43 -1000

    add colors, image file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 // shopping cart
 
+var cartContents = 
 [
     {product: "Finger Toothbrush", description: "A helping hand to a nicer smile.", price: 1.11}, {product: "Barry Manilow's Greatest Hits Collection Vol 1", description: "Music the way it should be! Reminisce the past glory as your ship to happiness has sailed.", price: 39.57}, {product: "Ramen Oreos", description: "The overly used cliche 'East Meets West' comes to life as Nabisco has infused the savory flavors of the Far East sandwiched between two chocolate wafers. Dip that in your sake.", price: 8.88}, {product: "Woof Washer 360", description: "Wash your dirty stinky mutt in minutes! Water and dog not included." , price: 9.29}, {product: "Sauna Pants", description: "Is it hot in here? Indeed. It's my pants. Look cool while losing weight.", price: 2.33}, {product: "Hug Me Pillow", description: "No more lonely nights as you snuggle with your best friend. And it will never walk out on you.", price: 599.99}
 ];
 
 // image, product, description, price
 
-
+function addImage(){
+    productsList.innerHTML = 
+    addImage(images/imagePlaceholder.png);
+}
+addItem()
